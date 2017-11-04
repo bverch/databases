f526af9263693c796b6ac5bf534883cd35130ecc
Hack Reactor Student <student@hackreactor.com>
2017-11-03 19:59:55 -0700

    be ready to be tested, to be ready to be coded

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,13 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+var dbConnection;
 
+dbConnection = mysql.createConnection({
+  user: 'root',
+  password: 'plantlife',
+  database: 'chat'
+});
+dbConnection.connect();
+
+module.exports.db = dbConnection;
